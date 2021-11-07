611370c1e4b3e8b9b4e3f4cdb0417c0b1c152ddc
alba02 <naomimaldo02@gmail.com>
2021-11-07 18:50:03 -0500

    Add parse

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cowsay from './utils/cowsay';
 
 dotenv.config();
+const prefix = process.env.PREFIX || '#am';
 
 const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -13,8 +14,22 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-  // Ping
-  if (message.content === 'ping') {
+  //prefix code
+  if (!message.content.startsWith(prefix)) {
+    return;
+  }
+
+  //arg code
+  const args = message.content
+    .toLowerCase()
+    .substring(prefix.length)
+    .slice()
+    .split(/ /);
+
+  const command = args.shift();
+
+  // Ping//
+  if (command === 'ping') {
     // React to a message with a unicode emoji
     message
       .react('🍒')
@@ -28,7 +43,7 @@ client.on('messageCreate', (message) => {
       .catch(console.error);
   }
   //cowsay
-  if (message.content === 'cowsay') {
+  if (command === 'cowsay') {
     message.react('🐱').then(console.log).catch(console.error);
     const output = cowsay();
     message.reply(output).then(console.log).catch(console.error);
