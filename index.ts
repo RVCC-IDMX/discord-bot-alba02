39edb9b55c707fc1a53fe41f69c23f3c39a8e566
alba02 <naomimaldo02@gmail.com>
2021-11-08 16:26:02 -0500

    Add channels

--- index.ts
+++ index.ts
@@ -4,6 +4,15 @@ import cowsay from './utils/cowsay';
 
 dotenv.config();
 const prefix = process.env.PREFIX || '#am';
+const CHANNELS = process.env.CHANNELS || null;
+
+if (!CHANNELS) {
+  console.error('CHANNELS is not defined');
+  process.exit(1);
+}
+
+const channels = CHANNELS.split(',');
+console.table(channels);
 
 const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -15,15 +24,17 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
   //prefix code
+  if (!channels.includes(message.channel.id)) return;
   if (!message.content.startsWith(prefix)) {
     return;
-  }
+  } else message.content.startsWith(prefix);
 
   //arg code
   const args = message.content
     .toLowerCase()
     .substring(prefix.length)
     .slice()
+    .trim()
     .split(/ /);
 
   const command = args.shift();
@@ -44,9 +55,15 @@ client.on('messageCreate', (message) => {
   }
   //cowsay
   if (command === 'cowsay') {
-    message.react('🐱').then(console.log).catch(console.error);
-    const output = cowsay();
-    message.reply(output).then(console.log).catch(console.error);
+    message
+      .react('🐱')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+    const output = cowsay(args[0]);
+    message
+      .reply(output)
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch(console.error);
   }
 });
 
--- utils/cowsay.ts
+++ utils/cowsay.ts
@@ -3,8 +3,10 @@ import { IOptions } from 'cowsay'; // optional
 import getRandomInt from './random';
 import quotes from './quotes.json';
 
+const options = cowsay.list;
+
 // Cowsay function
-export default function () {
+export default function (specialCow: string = 'any') {
   //create variable to represent the quotes and to place them in random generator
   const random = getRandomInt(0, quotes.length);
   let opts: IOptions = {
@@ -13,9 +15,19 @@ export default function () {
     r: true,
   };
 
-  let output: string = cowsay.say(opts);
+  if (specialCow != 'any') {
+    opts.r = false;
+    opts.f = specialCow;
+  }
 
-  console.log(output);
+  let output;
+  try {
+    output = cowsay.say(opts);
+  } catch (error) {
+    output = 'I am sorry, that cow does not exist.';
+  }
+
+  // Not random cow, specialcow
 
   if (output.length > 2000) {
     cowsay.say({ text: 'Bye, bye!' });
